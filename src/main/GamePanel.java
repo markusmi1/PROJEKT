f08bc3d56b8afa6959bdcd33992fdba34b5164bc
markusmi1 <michelis.markus@gmail.com>
2023-03-19 19:24:58 +0200

    JFrame

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,4 +1,20 @@
 package main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    int orgiginaalRuuduSuurus = 16;
+    int suhe = 3;
+    int ruuduSuurus = orgiginaalRuuduSuurus * suhe;
+    int maxLaiusRuute = 12;
+    int maxKõrgusRuute = 8;
+    int aknaLaius = maxLaiusRuute * ruuduSuurus;
+    int aknaKõrgus = maxKõrgusRuute * ruuduSuurus;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension( aknaLaius, aknaKõrgus));
+        this.setBackground(Color.green);
+        this.setDoubleBuffered(true);
+    }
 }
