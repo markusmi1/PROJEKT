3e90e940e134150d679a94f76e5095e23c628b50
RobinMurk <murkrobin@gmail.com>
2023-03-20 10:12:49 +0200

    esimene algne versioon Mainist ja peapaneeli klassist

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,4 +1,24 @@
 package main;
 
-public class GamePanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+
+    int blokiSuurus = 16;
+    int kordaja = 3;
+    final int lõplikSuurus = blokiSuurus * kordaja;
+    int ekraaniKõrgus = lõplikSuurus * 10;
+    int ekraaniLaius = lõplikSuurus * 15;
+
+    public GamePanel(){
+        this.setPreferredSize(new Dimension(ekraaniLaius,ekraaniKõrgus));
+        this.setBackground(Color.BLACK);
+        //this.setDoubleBuffered(true);
+    }
+
+
+
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,15 @@ public class Main {
     public static void main(String[] args) {
         JFrame aken = new JFrame();
         aken.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aken.setLocationRelativeTo(null);
+        aken.setResizable(false);
+        aken.setTitle("Totally not pokemon simulator");
+
+        GamePanel level = new GamePanel(); //uus mängu paneel
+        aken.add(level);  //lisab mängu paneeli aknasse
+        aken.pack();
+
+        aken.setVisible(true);
     }
 
 }
